437518dd832f48739bc7f0507d9af81f986050da
grailwilly <grailjwi11y@gmail.com>
2022-03-23 06:54:07 +0800

    Add media query max-width: 1320, max-width: 1200px, max-width: 992px, max-width: 912px, max width:768px, and max width: 412px

--- landing-page/javascript.js
+++ landing-page/javascript.js
@@ -1,11 +1,15 @@
 window.onscroll = function() {scrollFunction()};
 
+var x = window.matchMedia("(max-width: 992px");
+
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.getElementById("header").style.height = "10vh";
     document.getElementById("nav-bar").style.height = "10vh";
     document.getElementById("header-img").innerHTML = '<a href="index.html" alt="home" class="logo-text"><h1>Hyperlocal Market</h1></a>';
     document.querySelector("main").style.marginTop = "10%";
+  } else if(x.matches) {
+    document.getElementById("header").style.height = "auto";
   }else{
     document.getElementById("header").style.height = "40vh";
     document.getElementById("nav-bar").style.height = "40vh";
