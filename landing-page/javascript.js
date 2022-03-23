54f83435d1d6b64f534bf9d342c9277b060906b3
grailwilly <grailjwi11y@gmail.com>
2022-03-23 14:59:36 +0800

    Add recipe code file and responsive

--- landing-page/javascript.js
+++ landing-page/javascript.js
@@ -7,14 +7,14 @@ function scrollFunction() {
     document.getElementById("header").style.height = "10vh";
     document.getElementById("nav-bar").style.height = "10vh";
     document.getElementById("header-img").innerHTML = '<a href="index.html" alt="home" class="logo-text"><h1>Hyperlocal Market</h1></a>';
-    document.querySelector("main").style.marginTop = "10%";
+    document.querySelector("#homepage").style.marginTop = "10%";
   } else if(x.matches) {
     document.getElementById("header").style.height = "auto";
   }else{
     document.getElementById("header").style.height = "40vh";
     document.getElementById("nav-bar").style.height = "40vh";
     document.getElementById("header-img").innerHTML = '<a href="index.html" alt="home"><img src="assets/logo_pear.png" alt="Logo" class="logo"></a>';
-    document.querySelector("main").style.marginTop = "20%";
+    document.querySelector("#homepage").style.marginTop = "20%";
   }
 }
 
