a3c152c6b82f132e72f66b6fd657e183f01498a5
grailwilly <grailjwi11y@gmail.com>
2022-04-09 20:43:08 +0800

    Display user name and time

--- momentum-app-js/script.js
+++ momentum-app-js/script.js
@@ -1,4 +1,11 @@
 const displayGreeting = document.getElementById("user");
+const greetElement = document.createElement("h1");
+const userElement = document.createElement("h1");
+const displayClock = document.getElementById("time");
+const date = new Date();
+const hours = date.getHours();
+const minutes = date.getMinutes();
+const greetingType = ["Good Morning", "Good Afternoon", "Good Evening", "Get Some Sleep"]
 
 function greeting() {
   let urlString = window.location.search;
@@ -10,9 +17,27 @@ function greeting() {
   let getFirstName = url.get('firstName');
   console.log(getFirstName);
 
-  let greet = "Hi " + getFirstName;
+  let greet = "";
 
-  return greet;
+  if (hours < 12) greet = greetingType[0];
+  else if (hours < 18) greet = greetingType[1];
+  else if (hours > 24) greet = greetingType[3];
+  else greet = greetingType[2];
+
+  greetElement.innerText = greet + "!";
+  userElement.innerText = getFirstName;
+
+  // return greet + " " + getFirstName;
+
+  displayGreeting.append(greetElement, userElement)
+
 }
 
-displayGreeting.innerHTML = greeting();
+greeting();
+
+function time() {
+  displayClock.innerText = `${ hours < 10 ? `0${hours}` : hours}:${ minutes < 10 ? `0${minutes}` : minutes}`;
+}
+
+time();
+setInterval(time, 10000);
