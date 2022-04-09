ac8c2dc3c2522a10d2f54e98c840d459c9a7bd52
grailwilly <grailjwi11y@gmail.com>
2022-04-09 17:41:07 +0800

    Add javascript if name is not null redirect to homepage

---
+++ momentum-app-js/script.js
@@ -0,0 +1,17 @@
+const inputName = document.getElementById("first-name");
+const submitName = document.getElementById("submit-name");
+const formName = document.getElementById("input-name");
+
+submitName.addEventListener("click", function loginName(event) {
+  
+  event.preventDefault()
+
+  if(inputName.value !== "") {
+    console.log("Entered Name")
+    formName.submit();
+    inputName.value = "";
+  } else {
+    alert("Please enter your name")
+  }
+
+});
