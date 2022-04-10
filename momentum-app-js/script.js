335bc6a8c4299a21fe105799c0fc8b4aea8877f6
grailwilly <grailjwi11y@gmail.com>
2022-04-10 08:56:24 +0800

    Add button to quotes

--- momentum-app-js/script.js
+++ momentum-app-js/script.js
@@ -10,6 +10,8 @@ const focusLabel = document.getElementById("focus-label");
 const inputFocus = document.getElementById("input-focus");
 const addFocus = document.getElementById("add-focus");
 const clearFocus = document.getElementById("clear-focus");
+const quoteText = document.getElementById("quote-text");
+const authorName = document.getElementById("author");
 
 function greeting() {
   let urlString = window.location.search;
@@ -54,8 +56,63 @@ function resetFocus() {
   })
 }
 
+const quoteArray = [
+  {
+    quote: "&#8220; It is fine to celebrate success but it is more important to heed the lessons of failure. &#8221;",
+    author: "- Bill Gates, Co-founder of Microsoft"
+  },
+  {
+    quote: "&#8220; You cannot have everything you want, but you can have the things that really matter to you. &#8221;",
+    author: "- Marissa Mayer, President and CEO of Yahoo!"
+  },
+  {
+    quote: "&#8220; The biggest risk is not taking any risk… In a world that is changing really quickly, the only strategy that is guaranteed to fail, is not taking risks. &#8221;",
+    author: "- Mark Zuckerberg, Co-founder of Facebook"
+  },
+  {
+    quote: "&#8220; If you're offered a seat on a rocket ship, don't ask what seat. &#8221;",
+    author: "- Sheryl Sandberg, CEO of Lean In."
+  },
+  {
+    quote: "&#8220; If you don't innovate fast, disrupt your industry, disrupt yourself, you'll be left behind. &#8221;",
+    author: "- John Chambers, CEO of Cisco"
+  },
+  {
+    quote: "&#8220; Customers should be number one, Employees number two, and then only your Shareholders come at number three. &#8221;",
+    author: "- Jack Ma,  Founder, Alibaba"
+  },
+  {
+    quote: "&#8220; If you're competitor-focused, you have to wait until there is a competitor doing something. Being customer-focused allows you to be more pioneering. &#8221;",
+    author: "- Jeff Bezos, Founder of Amazon"
+  },
+  {
+    quote: "&#8220; Taking care of your employees is extremely important and very, very visible. &#8221;",
+    author: "- Larry Ellison, Founder of Oracle"
+  },
+  {
+    quote: "&#8220; The best way to predict the future is to invent it. &#8221;",
+    author: "- Alan Kay, computer scientist"
+  },
+  {
+    quote: "&#8220; Growth and comfort do not coexist. &#8221;",
+    author: "- Ginni Rometty, CEO of IBM"
+  },
+  {
+    quote: "&#8220; Keep going forward because success will come &#8221;",
+    author: "- Cassandra Sanford, CEO Kelly Mitchell Group"
+  }
+]
+
+function randomQuotes() {
+  let randomIndex = Math.floor(Math.random()*(quoteArray.length));
+
+  quoteText.innerHTML = quoteArray[randomIndex].quote;
+  authorName.innerHTML = quoteArray[randomIndex].author;
+}
+
 greeting();
 time();
 saveFocus();
 displayFocus();
 resetFocus();
+randomQuotes();
