4d80375ff111f1011b9ff49b0d59b7ffa293d4a5
grailwilly <grailjwi11y@gmail.com>
2022-04-12 11:08:36 +0800

    Add error message

--- momentum-app-js/login.js
+++ momentum-app-js/login.js
@@ -1,6 +1,8 @@
+const body = document.querySelector("body");
 const inputName = document.getElementById("first-name");
 const submitName = document.getElementById("submit-name");
 const formName = document.getElementById("input-name");
+const errorMessage = document.createElement("div");
 
 submitName.addEventListener("click", (event) => {
   
@@ -11,6 +13,21 @@ submitName.addEventListener("click", (event) => {
     localStorage.setItem("first-name", `${inputName.value}`);
     formName.submit();
   } else {
-    alert("Please enter your name!");
+    // alert("Please enter your name!");
+    const span = document.createElement("span");
+    const p = document.createElement("strong");
+    span.className = "closebtn";
+    errorMessage.className = "alert";
+    span.innerHTML = "&times;";
+    p.innerHTML = "Please Enter Name!"
+
+    errorMessage.append(span , p);
+
+    body.append(errorMessage);
+
+    span.addEventListener("click", () => {
+      errorMessage.style.display = "none";
+    });
+    
   }
 });
