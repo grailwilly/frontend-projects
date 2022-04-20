8c56a05437525c54bd851a045384c4dae6d74ffb
grailwilly <grailjwi11y@gmail.com>
2022-04-20 18:31:58 +0800

    Add functionalityjs

---
+++ tic-tac-toe/script.js
@@ -0,0 +1,89 @@
+/*----- constants -----*/
+
+const winningCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+
+/*----- app's state variables -----*/
+let board;
+
+let turn = 'X';
+
+let win;
+
+/*----- cached element references -----*/
+const squares = Array.from(document.querySelectorAll('#board div'));
+
+const messages = document.querySelector('h2');
+
+/*----- event listeners -----*/
+document.getElementById('board').addEventListener('click', handleTurn);
+
+document.getElementById('reset-button').addEventListener('click', init)
+
+/*----- functions -----*/
+
+function init() {
+  board = [
+    '', '', '',
+    '', '', '',
+    '', '', ''
+  ];
+
+  render();
+}
+
+function render() {
+  board.forEach((val, index) => {
+    // console.log(mark, index);
+    squares[index].textContent = val;
+  });
+
+  messages.textContent = win === 'T' ? `That's a tie, queen!` : win ? `${win} wins the game!` : `It's ${turn}'s turn!`
+}
+
+function handleTurn(event) {
+  let idx = squares.findIndex((square) => {
+    return square === event.target;
+  });
+
+  board[idx] = turn;
+
+  // console.log(board);
+
+  turn = turn === 'X' ? 'O' : 'X';
+
+  win = getWinner();
+  console.log(win);
+
+  render();
+};
+
+function getWinner() {
+
+  let winner = null;
+
+  winningCombos.forEach((combo, index) => {
+    if(board[combo[0]] && board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]]) {
+      winner = board[combo[0]];
+    }
+  });
+
+  return winner ? winner : board.includes('') ? null : 'T';
+  // if (winner) {
+  //   return winner;
+  // } else if (board.includes('')) {
+  //   return null 
+  // } else {
+  //   return T
+  // }
+};
+
+init();
