bb26ee96f5495a7df31ac4537d6b8188197897b8
grailwilly <grailjwi11y@gmail.com>
2022-04-23 12:38:39 +0800

    Add turn for each player

--- tic-tac-toe/main.js
+++ tic-tac-toe/main.js
@@ -1,2 +1,19 @@
 // X => <i class="fas fa-times"></i>
 // O => <i class="fas fa-circle-notch"></i>
+
+const mainPage = document.querySelector('#mainPage');
+const choosePlayer = document.querySelectorAll('.choose');
+
+let changeTurn = null;
+
+choosePlayer.forEach(player => {
+  player.addEventListener("click", () => {
+    if(player.id === "playerX") {
+      changeTurn = false;
+      // console.log(changeTurn);
+    }else{
+      changeTurn = true;
+      // console.log(changeTurn);
+    }
+  })
+})
