71fba2aa0b7b55e21503bb7bc9713894ded1967e
grailwilly <grailjwi11y@gmail.com>
2022-04-30 18:08:47 +0800

    Add event listener to sign up button displaying sign up form

--- bank-app/script.js
+++ bank-app/script.js
@@ -1,14 +1,19 @@
+// navbar
+const signUpBtn = document.querySelector(".sign-up");
+
 // heading
 const titleText = document.querySelector(".title");
 const descriptionText = document.querySelector(".description");
 
 // login form
+const loginForm = document.getElementById("login-form");
 const loginValue = document.getElementById("login-ID");
 const passwordValue = document.getElementById("password");
 const loginAccountBtn = document.getElementById("login-account");
 const forgetPassword = document.querySelector(".forget-password");
 
 // sign up form
+const signUpForm = document.getElementById("sign-up-form");
 const firstName = document.getElementById("first-name");
 const lastName = document.getElementById("last-name");
 const birthDate = document.getElementById("birth-date");
@@ -20,3 +25,10 @@ const phone = document.getElementById("phone");
 const username = document.getElementById("username");
 const newPassword = document.getElementById("create-password");
 const createAccountBtn = document.getElementById("create-account");
+
+
+signUpBtn.addEventListener("click", () => {
+  loginForm.style.display = "none";
+  signUpForm.style.display = "block";
+  titleText.innerText = "Create Your Account";
+})
