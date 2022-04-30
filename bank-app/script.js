3fa1af80231b8a2e357d5a9aa222ddba82c184b8
grailwilly <grailjwi11y@gmail.com>
2022-04-30 17:58:49 +0800

    Create variable for selectors and add id selectors

---
+++ bank-app/script.js
@@ -0,0 +1,22 @@
+// heading
+const titleText = document.querySelector(".title");
+const descriptionText = document.querySelector(".description");
+
+// login form
+const loginValue = document.getElementById("login-ID");
+const passwordValue = document.getElementById("password");
+const loginAccountBtn = document.getElementById("login-account");
+const forgetPassword = document.querySelector(".forget-password");
+
+// sign up form
+const firstName = document.getElementById("first-name");
+const lastName = document.getElementById("last-name");
+const birthDate = document.getElementById("birth-date");
+const genderSelect = document.getElementById("gender");
+const femaleValue = document.getElementById("female");
+const maleValue = document.getElementById("male");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const username = document.getElementById("username");
+const newPassword = document.getElementById("create-password");
+const createAccountBtn = document.getElementById("create-account");
