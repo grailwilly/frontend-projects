0aaefc15a2fac33080fd43a23cd11c0f28afce84
grailwilly <grailjwi11y@gmail.com>
2022-05-01 20:20:50 +0800

    Store users to array using localStorage

--- bank-app/script.js
+++ bank-app/script.js
@@ -14,21 +14,67 @@ const forgetPassword = document.querySelector(".forget-password");
 
 // sign up form
 const signUpForm = document.getElementById("sign-up-form");
-const firstName = document.getElementById("first-name");
-const lastName = document.getElementById("last-name");
-const birthDate = document.getElementById("birth-date");
-const genderSelect = document.getElementById("gender");
-const femaleValue = document.getElementById("female");
-const maleValue = document.getElementById("male");
-const email = document.getElementById("email");
-const phone = document.getElementById("phone");
-const username = document.getElementById("username");
-const newPassword = document.getElementById("create-password");
+const firstNameInput = document.getElementById("first-name");
+const lastNameInput = document.getElementById("last-name");
+const birthDateInput = document.getElementById("birth-date");
+const genderInput = document.getElementById("gender");
+const emailInput = document.getElementById("email");
+const phoneInput = document.getElementById("phone");
+const usernameInput = document.getElementById("username");
+const newPasswordInput = document.getElementById("create-password");
 const createAccountBtn = document.getElementById("create-account");
+
+// Profile object
+const userArray = [];
 
 
 signUpBtn.addEventListener("click", () => {
   loginForm.style.display = "none";
   signUpForm.style.display = "block";
   titleText.innerText = "Create Your Account";
+  // storeNewAccount();
 })
+
+createAccountBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  addUser(firstNameInput.value, lastNameInput.value, birthDateInput.value, genderInput.value, emailInput.value, phoneInput.value, usernameInput.value, newPasswordInput.value);
+});
+
+function addUser(fName, lName, birthDate, gender, email, phone, username, newPassword) {
+
+  if(fName !== '' && lName !== '' && birthDate !== '' && gender !== '' && email !== '' && phone !== '' && username !== '' && newPassword !== '') {
+    const userProfile = {
+      firstName: fName,
+      lastName: lName,
+      birthDate: birthDate,
+      gender: gender ,
+      email: email,
+      phone: phone,
+      username: username,
+      password: newPassword
+    };
+  
+    userArray.push(userProfile);
+    storeUsersToLocalStorage(userArray);
+    console.log(userArray);
+
+    firstNameInput.value = "";
+    lastNameInput.value = "";
+    birthDateInput.value = "";
+    genderInput.value = "";
+    emailInput.value = "";
+    phoneInput.value = "";
+    usernameInput.value = "";
+    newPasswordInput.value = "";
+
+    alert("Created Account");
+  } else {
+    alert("Please require fields");
+  }
+  
+  
+}
+
+function storeUsersToLocalStorage() {
+  localStorage.setItem('users', JSON.stringify(userArray));
+}
