53875ddc62de5dbb0cd851e6f3d38e64c5c39039
grailwilly <grailjwi11y@gmail.com>
2022-05-04 10:57:27 +0800

    Add deposit function

--- bank-app/js/account.js
+++ bank-app/js/account.js
@@ -1,8 +1,24 @@
 const logOutBtn = document.getElementById("logout-btn");
+const depositInput = document.getElementById("deposit-value");
 const depositBtn = document.querySelector(".deposit-btn");
 const withdrawBtn = document.querySelector(".deposit-btn");
 const sendMoneyBtn = document.querySelector(".deposit-btn");
 
 
 const users = JSON.parse(localStorage.getItem('users'));
-console.log(users);
+// console.log(users);
+
+depositBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  deposit(Number(depositInput.value));
+})
+
+function deposit(amount) {
+  for(let i = 0; i < users.length; i++) {
+    if(users[i].status === "Active") {
+      users[i].balance = Number(users[i].balance) + amount;
+      localStorage.setItem('users', JSON.stringify(users));
+      console.log(users)
+    }
+  }
+}
