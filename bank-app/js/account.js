06e94582d94252cda717c1e746acefd353908175
grailwilly <grailjwi11y@gmail.com>
2022-05-04 10:15:02 +0800

    Add log out button and add variables

--- bank-app/js/account.js
+++ bank-app/js/account.js
@@ -1,5 +1,8 @@
+const logOutBtn = document.getElementById("logout-btn");
+const depositBtn = document.querySelector(".deposit-btn");
+const withdrawBtn = document.querySelector(".deposit-btn");
+const sendMoneyBtn = document.querySelector(".deposit-btn");
+
 
 const users = JSON.parse(localStorage.getItem('users'));
 console.log(users);
-
-
