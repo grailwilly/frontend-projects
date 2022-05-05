405ead4bbc3f039361c5b6edca192b8ce40da3d3
grailwilly <grailjwi11y@gmail.com>
2022-05-06 01:56:30 +0800

    Add modals, css,and function

--- bank-app/js/account.js
+++ bank-app/js/account.js
@@ -1,6 +1,6 @@
 const logOutBtn = document.getElementById("logout-btn");
-const userName = document.querySelector(".name");
-const accountNumber = document.getElementById("account-number");
+const userName = document.querySelector(".name-display");
+const accountNumber = document.querySelector(".user-accountNumber");
 const balance = document.getElementById("balance")
 
 const depositInput = document.getElementById("deposit-value");
@@ -17,142 +17,121 @@ const sendMoneyLi = document.getElementById("sendMoney-transaction");
 
 const modalContainer = document.getElementById("modals");
 const depositModal = document.getElementById("deposit-modal");
+const withdrawalModal = document.getElementById("withdrawal-modal");
+const sendMoneyModal = document.getElementById("sendMoney-modal");
 const depositHeading = document.querySelector(".deposit-heading");
 const depositModalInput = document.querySelector(".deposit-input");
-const closeModal = document.querySelector(".close-modal");
+const closeModals = document.querySelectorAll(".close-modal");
 
 const users = JSON.parse(localStorage.getItem('users'));
 // console.log(users);
 
-// depositBtn.addEventListener("click", (event) => {
-//   event.preventDefault();
-//   deposit(Number(depositInput.value));
-// })
+depositBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  deposit(Number(depositInput.value));
+})
 
-// withdrawBtn.addEventListener("click", (event) => {
-//   event.preventDefault();
-//   withdraw(Number(withdrawInput.value));
-// })
+withdrawBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  withdraw(Number(withdrawInput.value));
+})
 
-// sendMoneyBtn.addEventListener("click", (event) => {
-//   event.preventDefault();
-//   sendMoney(Number(sendAmountInput.value), Number(sendAccountNumberInput.value));
-// })
+sendMoneyBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  sendMoney(Number(sendAmountInput.value), Number(sendAccountNumberInput.value));
+})
 
-// logOutBtn.addEventListener("click", () => {
-//   for(let i = 0; i < users.length; i++) {
-//     delete users[i].status;
-//     localStorage.setItem('users', JSON.stringify(users));
-//     window.location = "../index.html";
-//     console.log(users)
-//   }
-// })
+logOutBtn.addEventListener("click", () => {
+  for(let i = 0; i < users.length; i++) {
+    delete users[i].status;
+    localStorage.setItem('users', JSON.stringify(users));
+    window.location = "../index.html";
+    console.log(users)
+  }
+})
 
-// function displayUser() {
-//   for(let i = 0; i < users.length; i++) {
-//     if(users[i].status === "Active") {
-//       userName.innerText = users[i].firstName;
-//       accountNumber.innerText = users[i].accountNumber;
-//       balance.innerText = users[i].balance;
-//     }
-//   }
-// }
+function displayUser() {
+  for(let i = 0; i < users.length; i++) {
+    if(users[i].status === "Active") {
+      userName.innerText = users[i].firstName;
+      accountNumber.innerText = users[i].accountNumber;
+      balance.innerText = users[i].balance;
+    }
+  }
+}
 
-// function deposit(amount) {
-//   for(let i = 0; i < users.length; i++) {
-//     if(users[i].status === "Active") {
-//       users[i].balance = Number(users[i].balance) + amount;
-//       localStorage.setItem('users', JSON.stringify(users));
-//       depositInput.value = "";
-//       console.log(users)
-//     }
-//   }
-// }
+function deposit(amount) {
+  for(let i = 0; i < users.length; i++) {
+    if(users[i].status === "Active") {
+      users[i].balance = Number(users[i].balance) + amount;
+      localStorage.setItem('users', JSON.stringify(users));
+      depositInput.value = "";
+      console.log(users)
+    }
+  }
+}
 
-// function withdraw(amount) {
-//   for(let i = 0; i < users.length; i++) {
-//     if(users[i].status === "Active") {
-//       users[i].balance = Number(users[i].balance) - amount;
-//       localStorage.setItem('users', JSON.stringify(users));
-//       withdrawInput.value = "";
-//       console.log(users)
-//     }
-//   }
-// }
+function withdraw(amount) {
+  for(let i = 0; i < users.length; i++) {
+    if(users[i].status === "Active") {
+      users[i].balance = Number(users[i].balance) - amount;
+      localStorage.setItem('users', JSON.stringify(users));
+      withdrawInput.value = "";
+      console.log(users)
+    }
+  }
+}
 
-// function sendMoney(amount, accountNumber) {
-//   for(let i = 0; i < users.length; i++) {
-//     if(users[i].status === "Active") {
-//       users[i].balance = Number(users[i].balance) - amount;
-//       localStorage.setItem('users', JSON.stringify(users));
-//       sendAmountInput.value = "";
-//       sendAccountNumberInput.value = "";
-//       console.log(users)
-//     } 
+function sendMoney(amount, accountNumber) {
+  for(let i = 0; i < users.length; i++) {
+    if(users[i].status === "Active") {
+      users[i].balance = Number(users[i].balance) - amount;
+      localStorage.setItem('users', JSON.stringify(users));
+      sendAmountInput.value = "";
+      sendAccountNumberInput.value = "";
+      console.log(users)
+    } 
 
-//     if(users[i].accountNumber === Number(accountNumber)) {
-//       users[i].balance = Number(users[i].balance) + amount;
-//       localStorage.setItem('users', JSON.stringify(users));
-//       console.log(users)
-//     }
+    if(users[i].accountNumber === Number(accountNumber)) {
+      users[i].balance = Number(users[i].balance) + amount;
+      localStorage.setItem('users', JSON.stringify(users));
+      console.log(users)
+    }
       
-//   }
-// }
+  }
+}
 
-// displayUser();
+displayUser();
 
 
 /* modals */
 depositLi.addEventListener("click", (event) => {
   event.preventDefault();
   modalContainer.style.display = "block";
-
-  depositHeading.innerHTML = `
-    <h1>Deposit</h1>
-    <p>Save money on your account.</p>
-  `
-
-  depositModalInput.innerHTML = `
-    <input type="text" id="deposit-value" placeholder="Type amount" />
-    <button class="deposit-btn">Deposit</button>
-  `
-  closeModal.style.top = "-290px";
+  depositModal.style.display = "block";
+  withdrawalModal.style.display = "none";
+  sendMoneyModal.style.display = "none";
 });
 
 withdrawLi.addEventListener("click", (event) => {
   event.preventDefault();
   modalContainer.style.display = "block";
-
-  depositHeading.innerHTML = `
-  <h1>Withdraw</h1>
-  <p>Spend money from your account</p>
-  `
-
-  depositModalInput.innerHTML = `
-    <input type="text" id="withdraw-value" placeholder="Type Amount" />
-    <button class="withdraw-btn">Widthraw</button>
-  `
-  closeModal.style.top = "-290px";
+  withdrawalModal.style.display = "block";
+  depositModal.style.display = "none";
+  sendMoneyModal.style.display = "none";
 })
 
 sendMoneyLi.addEventListener("click", (event) => {
   event.preventDefault();
   modalContainer.style.display = "block";
-
-  depositHeading.innerHTML = `
-  <h1>Send Money</h1>
-  <p>Donate 2% from your account.</p>
-  `
-
-  depositModalInput.innerHTML = `
-  <input type="text" id="send-amount" placeholder="Type amount" />
-  <input type="text" id="send-account-number" placeholder="Recepient's account number" />
-  <button class="send-money-btn">Send Money</button>
-  `
-  closeModal.style.top = "-350px";
+  sendMoneyModal.style.display = "block";
+  depositModal.style.display = "none";
+  withdrawalModal.style.display = "none";
 })
 
-closeModal.addEventListener("click", (event) => {
-  event.preventDefault();
-  modalContainer.style.display = "none";
+
+closeModals.forEach((closeModal) => {
+  closeModal.addEventListener("click", () => {
+    modalContainer.style.display = "none";
+  })
 })
