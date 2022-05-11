4a25af95841c5e040256e40f75fda6a539ac5177
grailwilly <grailjwi11y@gmail.com>
2022-05-11 20:25:54 +0800

    Add doughnut chart

--- bank-app/js/budget.js
+++ bank-app/js/budget.js
@@ -82,4 +82,34 @@ window.onload = function() {
   displayExpenses();
 }
 
+// pie chart
+
+const ctx = document.getElementById("chart").getContext('2d');
+
+var xValues = ["Expenses", "Savings", "Investments"];
+var yValues = [55, 49, 44];
+var barColors = [
+  "#b91d47",
+  "#00aba9",
+  "#2b5797"
+];
+
+new Chart(ctx, {
+  type: "doughnut",
+  data: {
+    labels: xValues,
+    datasets: [{
+      backgroundColor: barColors,
+      data: yValues
+    }]
+  }
+  // options: {
+  //   title: {
+  //     display: true,
+  //     text: "World Wide Wine Production 2018"
+  //   }
+  // }
+});
+
+
 
