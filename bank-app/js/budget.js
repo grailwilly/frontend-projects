8d1a1c05a59020957f5a3ccff240d465f4819dd7
grailwilly <grailjwi11y@gmail.com>
2022-05-13 17:47:42 +0800

    Add deletefunction expenses

--- bank-app/js/budget.js
+++ bank-app/js/budget.js
@@ -11,8 +11,6 @@ let year = date.getFullYear();
 
 const expenseTable = document.getElementById("expense-row");
 
-const expenseArray = [];
-
 const Expense = function(expenseType, description, amount){
   let dateNow = `${month} ${day}, ${year}`;
   let id = Math.floor(Math.random() * 100);
@@ -28,15 +26,16 @@ addExpenseBtn.addEventListener("click", (event) => {
   addExpense(expenseTypeInput.value, expenseDescriptionInput.value, Number(expenseAmountInput.value));
   let currentAccount = JSON.parse(localStorage.getItem("currentAccount"));
   let lastItem = currentAccount.expenses[currentAccount.expenses.length - 1];
-   const tr = document.createElement("tr");
-       tr.innerHTML = `
+   const trItem = document.createElement("tr");
+       trItem.innerHTML = `
         <td id="exp-date">${lastItem.dateNow}</td>
         <td id="exp-type">${lastItem.expenseType}</td>
         <td id="exp-description">${lastItem.description}</td>
         <td id="exp-amount">${lastItem.amount}</td>
-        <td id="exp-action">Edit | Delete</td>
+        <td id="exp-Edit"><button>Edit</button></td>
+        <td id="exp-delete" data-key=${lastItem.id}><button>Delete</button></td>
        `
-    expenseTable.prepend(tr);
+    expenseTable.prepend(lastItem);
   window.location.reload();
 })
 
@@ -65,14 +64,15 @@ function displayExpenses() {
   let currentAccount = JSON.parse(localStorage.getItem("currentAccount"));
   let expenses = currentAccount.expenses.reverse();
     for(let j = 0; j < expenses.length; j++) {
-       const tr = document.createElement("tr");
-       tr.setAttribute('data-key', expenses[j].id)
-       tr.innerHTML = `
+      const tr = document.createElement("tr");
+      tr.className = "expenses-tr";
+      tr.innerHTML = `
         <td id="exp-date">${expenses[j].dateNow}</td>
         <td id="exp-type">${expenses[j].expenseType}</td>
         <td id="exp-description">${expenses[j].description}</td>
         <td id="exp-amount">${expenses[j].amount.toLocaleString('en')}</td>
-        <td id="exp-action"><span class="edit-exp">Edit</span> | <span class="delete-exp">Delete</span></td>
+        <td id="exp-Edit"><button>Edit</button></td>
+        <td id="exp-delete" data-key=${expenses[j].id}><button>Delete</button></td>
        `
        expenseTable.append(tr);
     }
@@ -80,4 +80,24 @@ function displayExpenses() {
 
 window.onload = function() {
   displayExpenses();
+
+  const deleteExpBtns = document.querySelectorAll("#exp-delete");
+
+  deleteExpBtns.forEach(function (deleteBtn){
+    deleteBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      let currentAccount = JSON.parse(localStorage.getItem("currentAccount"));
+      let expenses = currentAccount.expenses;
+      let index = Array.from(expenses).findIndex(expense => {
+        return expense.id === Number(deleteBtn.getAttribute("data-key"))
+      })
+
+      if( index > -1) {
+        expenses.splice(index, 1);
+      }
+
+      localStorage.setItem("currentAccount", JSON.stringify(currentAccount));
+      window.location.reload();
+    })
+  })
 }
